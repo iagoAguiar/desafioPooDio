0d467e7a48dc52af49f05de985831f95f09279d1
iagoAguiar <iagoaguiar202@gmail.com>
2021-12-11 20:06:36 -0300

    first commit

---
+++ src/br/com/dio/desafio/Main.java
@@ -0,0 +1,13 @@
+package br.com.dio.desafio;
+
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso = new Curso();
+		
+
+	}
+
+}
